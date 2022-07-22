e7d1a274e4785ac9728b183dd43c04e2072cf612
gelmutyansky <rgelmut@yandex.ru>
2022-07-22 22:55:33 +0500

    Загрузка множественного файла

--- handlers/objects/multiple/handler.js
+++ handlers/objects/multiple/handler.js
@@ -1,3 +1,74 @@
-module.exports = {
+const { pool, commonErrors } = require('../../../dependes');
+const fs = require('fs/promises');
 
+/**
+ * Загрузка множественного файла
+ * @param { array } object.file
+ * @param { Buffer } object.file[0].data
+ * @param { string } object.file[0].filename
+ * @param { number } object.objectId
+ * @returns Promise<{message: {fileUrl: string}| string, statusCode: number}>
+ * */
+async function uploadFile(object) {
+    let data = {
+        message:    commonErrors.default,
+        statusCode: 400,
+    };
+    const client = await pool.connect();
+    const funcName = 'uploadFile';
+    const failed = `${ funcName }: failed.`;
+
+    try {
+        const queryExistObject = `SELECT "objectId"
+                                  FROM objects
+                                  WHERE "objectId" = $1`;
+        const resExistObject = await client.query(queryExistObject, [ object.objectId ]);
+
+        if (resExistObject.rows.length > 0) {
+            const timestamp = +(new Date());
+
+            const { data: fileData, filename } = object.file[0];
+
+            const path = `./public/${ timestamp }${ filename.replace(/ /g, '_') }`;
+
+            await fs.writeFile(path, fileData);
+
+            const queryInsert = `INSERT INTO files ("objectId", "fileUrl")
+                                 VALUES ($1, $2)`;
+            const resInsert = await client.query(queryInsert, [ object.objectId, path ]);
+
+            if (resInsert.rowCount > 0) {
+                data = {
+                    message:    {
+                        fileUrl: path,
+                    },
+                    statusCode: 200,
+                };
+            }
+            else {
+                console.log(`${ failed } File not inserted. Deleting file... (objectId: ${ object.objectId }, path: ${ path })`);
+
+                await fs.unlink(path);
+            }
+        }
+        else {
+            data.message = commonErrors.noObject;
+        }
+    } catch (e) {
+        console.error(e.stack, e.message);
+        data = {
+            message:    e.message,
+            statusCode: 400,
+        };
+    } finally {
+        client.release();
+        console.log('Release client');
+    }
+
+    return data;
+}
+
+
+module.exports = {
+    uploadFile: uploadFile,
 };
--- routes/objects/multiple/route.js
+++ routes/objects/multiple/route.js
@@ -1,5 +1,41 @@
 const job = require('../../../handlers/objects/multiple/handler');
 
 module.exports = function (fastify, opts, next) {
+    fastify.route({
+        method: 'POST',
+        url:    '/upload',
+        schema: {
+            body:     {
+                type:       'object',
+                properties: {
+                    objectId: { type: 'integer' },
+                    file:     {
+                        type:  'array',
+                        items: fastify.getSchema('MultipartFileType'),
+                    },
+                },
+                required:   [ 'objectId', 'file' ],
+            },
+            response: {
+                400: {
+                    type:       'object',
+                    properties: {
+                        message:    { type: 'string' },
+                        statusCode: { type: 'integer' },
+                    },
+                },
+            },
+        },
+        async handler(request, reply) {
+            let data = await job.uploadFile(request.body);
+
+            if (data.statusCode !== 200) {
+                reply.status(400);
+            }
+
+            reply.send(data);
+        },
+    });
+
     next();
 };
