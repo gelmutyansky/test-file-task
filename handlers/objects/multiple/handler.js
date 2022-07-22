5d4f639519a60040cd2c3b566e46a2c652b794b2
gelmutyansky <rgelmut@yandex.ru>
2022-07-22 23:23:55 +0500

    Удаление файла

--- handlers/objects/multiple/handler.js
+++ handlers/objects/multiple/handler.js
@@ -66,7 +66,79 @@ async function uploadFile(object) {
     return data;
 }
 
+/**
+ * @param { number } object.objectId
+ * @param { number } object.fileId
+ * */
+async function deleteFile(object) {
+    let data = {
+        message:    commonErrors.default,
+        statusCode: 400,
+    };
+    const client = await pool.connect();
+    const funcName = 'deleteFile';
+    const failed = `${ funcName }: failed.`;
+
+    try {
+        const querySelectFile = `SELECT "fileUrl"
+                                 FROM files
+                                 WHERE "objectId" = $1
+                                   AND "fileId" = $2`;
+        const resSelectFile = await client.query(querySelectFile, [ object.objectId, object.fileId ]);
+
+        if (resSelectFile.rows.length > 0) {
+            const { fileUrl } = resSelectFile.rows[0];
+
+            const queryDelete = `DELETE
+                                 FROM files
+                                 WHERE "objectId" = $1
+                                   AND "fileId" = $2`;
+            const resDelete = await client.query(queryDelete, [ object.objectId, object.fileId ]);
+
+            if (resDelete.rowCount > 0) {
+                data = {
+                    message:    {
+                        success: true,
+                    },
+                    statusCode: 200,
+                };
+
+                if (fileUrl) {
+                    // Попробуем удалить файл. А если не получится, то пользователю не обязательно знать
+                    try {
+                        await fs.unlink(fileUrl);
+                    } catch (e) {
+                        console.log(`${ funcName }: file not deleted. Delete manually later!`);
+                        console.log(`error message: ${ e.message }`);
+                    }
+                }
+                else {
+                    // Путь до файла пустой. Странно, но ладно
+                }
+            }
+            else {
+                console.log(`${ failed } File not deleted from (objectId: ${ object.objectId }, fileId: ${ object.fileId })`);
+            }
+        }
+        else {
+            data.message = commonErrors.noObject;
+        }
+    } catch (e) {
+        console.error(e.stack, e.message);
+        data = {
+            message:    e.message,
+            statusCode: 400,
+        };
+    } finally {
+        client.release();
+        console.log('Release client');
+    }
+
+    return data;
+}
+
 
 module.exports = {
     uploadFile: uploadFile,
+    deleteFile: deleteFile,
 };
