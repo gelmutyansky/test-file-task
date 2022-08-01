745c3898a879d43615b70bc8c5e4bd9e4ee1a858
gelmutyansky <rgelmut@yandex.ru>
2022-08-01 22:13:28 +0500

    Запросы для создания таблиц

---
+++ tables.sql
@@ -0,0 +1,34 @@
+-- таблица объектов
+create table objects
+(
+    "objectId"   serial
+        constraint objects_pk
+            primary key,
+    "fileUrl"    text,
+    "objectName" text
+);
+
+-- alter table objects
+--     owner to gelmutr;
+
+create unique index objects_objectid_uindex
+    on objects ("objectId");
+
+-- таблица (множественных) файлов
+create table files
+(
+    "fileId"   serial
+        constraint files_pk
+            primary key,
+    "objectId" integer not null
+        constraint files_objects_objectid_fk
+            references objects
+            on delete set null,
+    "fileUrl"  text    not null
+);
+
+-- alter table files
+--     owner to gelmutr;
+
+create unique index files_fileid_uindex
+    on files ("fileId");
